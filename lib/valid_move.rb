37941152947e39006bbc34b81ab0285add809d07
Melissa Rudick <mel.madrugada@gmail.com>
2017-08-30 01:00:22 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,11 +1,11 @@
-# code your #valid_move? method here
-# def position_taken?(board,index)
-#   if board[index] == "X" || board[index] == "O"
-#     true
-#   else
-#     false
-# end
-# end
+code your #valid_move? method here
+def position_taken?(board,index)
+  if board[index] == "X" || board[index] == "O"
+    true
+  else
+    false
+end
+end
 
 def valid_move?(board,index)
   if !position_taken?(board,index) && index.between?(0,8)
